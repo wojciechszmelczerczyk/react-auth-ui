217d3b62f2ef9e93969e63b6c561d539160bf4b9
Wojciech <szmelczerczykwojciech@gmail.com>
2023-03-28 18:47:36 +0200

    working on homepage ui

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,50 @@
 import { Navigate, useParams } from "react-router";
+import { useNavigate } from "react-router-dom";
+import finances from "../img/Finances (1).svg";
 
 const HomePage = () => {
   const { user } = useParams();
   const jwt = localStorage.getItem("jwt");
+  const navigate = useNavigate();
 
-  return <>{jwt ? <h1>Hello {user}</h1> : <Navigate to='/signin' />}</>;
+  const logout = () => {
+    localStorage.removeItem("jwt");
+    localStorage.removeItem("email");
+    navigate("/signin");
+  };
+
+  return (
+    <>
+      {jwt ? (
+        <div className='flex flex-col bg-pink h-screen w-screen items-center overflow-hidden '>
+          <div className='flex flex-col items-center'>
+            <p className='text-orange_form h-32 pt-28 text-5xl font-poppins font-semibold'>
+              Logo here
+            </p>
+            <h1 className='font-poppins text-6xl pt-24'>
+              Hello{" "}
+              <p className='inline-block font-poppins text-6xl font-bold'>
+                {user}!
+              </p>
+            </h1>
+            <button
+              className='flex text-white justify-center bg-orange_form hover:bg-hover_orange_form rounded-3xl w-36 h-12 my-10'
+              onClick={logout}
+            >
+              <p className='font-poppins font-semibold mx-3 self-center text-md'>
+                Sign out
+              </p>
+            </button>
+          </div>
+          <div className='flex'>
+            <img alt='' src={finances} width={450} />
+          </div>
+        </div>
+      ) : (
+        <Navigate to='/signin' />
+      )}
+    </>
+  );
 };
 
 export default HomePage;
