4e2918983f93947224fcadc38210963c5480f4c0
Wojciech <szmelczerczykwojciech@gmail.com>
2023-03-28 17:57:37 +0200

    get rid of unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import Router from "./Router";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [user, setUser] = useState<String>();
-
   return (
     <BrowserRouter>
       <Router />
